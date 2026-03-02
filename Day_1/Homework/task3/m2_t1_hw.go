bda2bd07aacba70b57bf8dbf25b38cd65018f388
Александр <terehov363@gmail.com>
2026-03-02 13:42:50 +0400

    Третье задание

--- Day_1/Homework/task3/m2_t1_hw.go
+++ Day_1/Homework/task3/m2_t1_hw.go
@@ -9,4 +9,43 @@
 */
 package main
 
-// Ваш код
+import (
+	"fmt"
+)
+
+func main() {
+	var x, y, z int
+
+	fmt.Println("Введите три числа для сортировки: ")
+	_, err := fmt.Scan(&x, &y, &z)
+
+	/*
+		Хочу вот так:
+		var testArr []string
+		_, err := fmt.Scan(&testArr)
+		fmt.Printf("Тестовый массив: %v\n", testArr)
+	*/
+
+	if err != nil {
+		return
+	}
+
+	numbers := []int{x, y, z}
+
+	var sortArr []int
+	sortArr = bubbleSort(numbers)
+	fmt.Printf("Отсортированный массив: %v\n", sortArr)
+}
+
+func bubbleSort(arr []int) []int {
+	n := len(arr)
+
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr
+}
