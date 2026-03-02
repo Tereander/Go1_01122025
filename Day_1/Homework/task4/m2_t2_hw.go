0a21e67a4c48136ad2c0e164fd22d4a610200a18
Александр <terehov363@gmail.com>
2026-03-02 14:07:10 +0400

    Четвертое задание

--- Day_1/Homework/task4/m2_t2_hw.go
+++ Day_1/Homework/task4/m2_t2_hw.go
@@ -6,4 +6,29 @@
 */
 package main
 
-// Ваш код
+import "fmt"
+
+func main() {
+	var number int
+	fmt.Print("Введите четырехзначное число: ")
+	_, err := fmt.Scan(&number)
+	if err != nil {
+		return
+	}
+
+	// Проверяем, что число четырехзначное
+	if number < 1000 || number > 9999 {
+		fmt.Println("Ошибка: число должно быть четырехзначным")
+		return
+	}
+
+	// Преобразуем число в строку
+	str := fmt.Sprintf("%d", number)
+
+	// Проверяем, является ли число палиндромом
+	if str[0] == str[3] && str[1] == str[2] {
+		fmt.Printf("%d - палиндром\n", number)
+	} else {
+		fmt.Printf("%d - не палиндром\n", number)
+	}
+}
