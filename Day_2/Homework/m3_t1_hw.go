923f9affbf4148f5b4f75753d9cdb51ae4747993
Александр <terehov363@gmail.com>
2026-03-03 15:04:52 +0300

    День два

--- Day_2/Homework/m3_t1_hw.go
+++ Day_2/Homework/m3_t1_hw.go
@@ -27,5 +27,60 @@ In: 5 5 5 5 5     Out: CONSTANT
 
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-// Ваш код
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Введите последовательность из 5 чисел: ")
+
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+
+	parts := strings.Split(input, " ")
+
+	numbersArr := make([]int, len(parts))
+
+	for i, part := range parts {
+		num, _ := strconv.Atoi(part)
+		numbersArr[i] = num
+	}
+
+	result := determineSequenceType(numbersArr)
+	fmt.Println(result)
+
+}
+
+func determineSequenceType(nums []int) string {
+	hasIncrease := false
+	hasDecrease := false
+	hasEqual := false
+
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] < nums[i+1] {
+			hasIncrease = true
+		} else if nums[i] > nums[i+1] {
+			hasDecrease = true
+		} else if nums[i] == nums[i+1] {
+			hasEqual = true
+		}
+	}
+	if hasIncrease && !hasDecrease && !hasEqual {
+		return "ASCENDING"
+	} else if hasIncrease && !hasDecrease && hasEqual {
+		return "WEAKLY ASCENDING"
+	} else if !hasIncrease && hasDecrease && !hasEqual {
+		return "DESCENDING"
+	} else if !hasIncrease && hasDecrease && hasEqual {
+		return "WEAKLY DESCENDING"
+	} else if !hasIncrease && !hasDecrease && hasEqual {
+		return "CONSTANT"
+	} else {
+		return "RANDOM"
+	}
+}
